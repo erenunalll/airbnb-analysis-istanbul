1715345caa74040c5880e1da83ffaefeba4ffd65
Eren Ünal <ernunl719@gmail.com>
2026-05-11 21:46:46 +0300

    measures of centraltendency & variation

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,11 +1,50 @@
 # load the data frame
 df <- read.csv("./data/airbnb.csv")
 
-# Central tendencies
+# check for missing values
+sum(is.na(df)) # 0
+
+# Measures of Central Tendency
 mean_price <- mean(df$price, na.rm = TRUE)
 median_price <- median(df$price, na.rm = TRUE)
 get_mode <- function(x) {
-  unique_values <- unique(x)
+  unique_values <- unique(x) 
   unique_values[which.max(tabulate(match(x, unique_values)))]
 }
+
 mode_price <- get_mode(df$price)
+
+# Measures of Variation
+range <- diff(range(df$price))
+var_price <- var(df$price)
+sd_price <- sd(df$price)
+cvar_price <- (sd_price / mean_price)*100
+
+
+cat(
+  "MEASURES OF CENTRAL TENDENCY\n",
+  "mean  : ", mean_price, "\n",
+  "median: ", median_price, "\n",
+  "mode  :", mode_price, "\n"
+)
+cat(
+  "MEASURES OF VARIATION\n",
+  "variation               : ", var_price, "\n",
+  "standard deviation      : ", sd_price, "\n",
+  "coefficient of variation:", cvar_price, "\n"
+)
+
+# Histogram of price
+hist(df$price,
+     col = "lightyellow",
+     main = "Histogram of Airbnb Prices in Istanbul",
+     xlab = "Prices of Airbnb in Istanbul")
+
+# Box plot of price
+boxplot(df$price,
+        col = "lightyellow",
+        main = "Boxplot of Airbnb Prices in Istanbul",
+        xlab = "Prices of Airbnb in Istanbul",
+        outline = FALSE,
+        horizontal = TRUE)
+
